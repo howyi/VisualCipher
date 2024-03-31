9612783c99c1a5f1777706afd669e7fe7e2c90d4
t-hayashi <t.hayashi.dev@gmail.com>
2024-03-31 12:00:43 +0900

    add GA

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -35,6 +35,16 @@ const config: GatsbyConfig = {
         display: `standalone`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ['G-0JN5T11B6V'],
+        pluginConfig: {
+          head: true,
+        },
+      },
+    },
   ],
 }
 
