4ff6617cf891352a40cd77e50d37eedf3116e277
t-hayashi <t.hayashi.dev@gmail.com>
2024-03-31 12:03:54 +0900

    update pwa-install button

--- src/components/organisms/chrome-app-install-button.tsx
+++ src/components/organisms/chrome-app-install-button.tsx
@@ -2,5 +2,11 @@ import * as React from 'react'
 import PWAInstall from '@/react-pwa-install/pwa-install'
 
 export function ChromeAppInstallButton() {
-  return <PWAInstall manifest-url="/manifest.webmanifest" />
+  return (
+    <PWAInstall
+      manifest-url="/manifest.webmanifest"
+      manual-apple={'true'}
+      manual-chrome={'true'}
+    />
+  )
 }
